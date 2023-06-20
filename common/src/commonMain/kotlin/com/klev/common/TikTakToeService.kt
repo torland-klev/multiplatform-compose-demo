43b4da26ef706fb8d7f79e47e0fe1a60a026a3c9
Henrik Klev <klevhenrik@gmail.com>
2023-06-20 09:22:42 +0200

    Fixed stupid stuff

--- common/src/commonMain/kotlin/com/klev/common/TikTakToeService.kt
+++ common/src/commonMain/kotlin/com/klev/common/TikTakToeService.kt
@@ -18,7 +18,7 @@ object TikTakToeService {
         val checkDiagonalUpDown = checkDiagonalUpDown(state)
         val checkDiagonalDownUp = checkDiagonalDownUp(state)
         return if (rows == BoardState.VICTORY_X || columns == BoardState.VICTORY_X || checkDiagonalUpDown == BoardState.VICTORY_X || checkDiagonalDownUp == BoardState.VICTORY_X) BoardState.VICTORY_X
-        else if (rows == BoardState.VICTORY_Y || columns == BoardState.VICTORY_Y || checkDiagonalUpDown == BoardState.VICTORY_Y || checkDiagonalDownUp == BoardState.VICTORY_Y) BoardState.VICTORY_Y
+        else if (rows == BoardState.VICTORY_O || columns == BoardState.VICTORY_O || checkDiagonalUpDown == BoardState.VICTORY_O || checkDiagonalDownUp == BoardState.VICTORY_O) BoardState.VICTORY_O
         else if (state.squares.any { it.symbol == Symbol.EMPTY }) BoardState.IN_PROGRESS
         else BoardState.DRAW
     }
@@ -31,14 +31,14 @@ object TikTakToeService {
     private fun checkDiagonalUpDown(state: TikTakToeState) = state.squares.filter { it.x == it.y }.check()
 
     private fun checkDiagonalDownUp(state: TikTakToeState) = state.squares.filter { it.x + it.y == 2 }.check()
-    private fun List<Square>.check() = if (this.all { it.symbol == Symbol.CROSS }) BoardState.VICTORY_X else if (this.all { it.symbol == Symbol.CROSS }) BoardState.VICTORY_Y else if (this.any { it.symbol == Symbol.EMPTY }) BoardState.IN_PROGRESS else BoardState.DRAW
-    private fun Map<Int, List<Square>>.check() = if (this.any { it.value.all { it.symbol == Symbol.CROSS } }) BoardState.VICTORY_X else if (this.any { it.value.all { it.symbol == Symbol.CIRCLE } }) BoardState.VICTORY_Y else if (this.any { it.value.any { it.symbol == Symbol.EMPTY } }) BoardState.IN_PROGRESS else BoardState.DRAW
+    private fun List<Square>.check() = if (this.all { it.symbol == Symbol.CROSS }) BoardState.VICTORY_X else if (this.all { it.symbol == Symbol.CIRCLE }) BoardState.VICTORY_O else if (this.any { it.symbol == Symbol.EMPTY }) BoardState.IN_PROGRESS else BoardState.DRAW
+    private fun Map<Int, List<Square>>.check() = if (this.any { it.value.all { it.symbol == Symbol.CROSS } }) BoardState.VICTORY_X else if (this.any { it.value.all { it.symbol == Symbol.CIRCLE } }) BoardState.VICTORY_O else if (this.any { it.value.any { it.symbol == Symbol.EMPTY } }) BoardState.IN_PROGRESS else BoardState.DRAW
 
     fun bannerText(state: TikTakToeState) = when (checkState(state)) {
             BoardState.DRAW -> "DRAW"
             BoardState.IN_PROGRESS -> ""
             BoardState.VICTORY_X -> "Congratulations, X!"
-            BoardState.VICTORY_Y -> "Congratulations, O!"
+            BoardState.VICTORY_O -> "Congratulations, O!"
     }
 
     fun update(state: TikTakToeState, square: Square, xTurn: Boolean): TikTakToeState = state.copy(squares = state.squares.filterNot { it == square } + square.copy(symbol = if (xTurn) Symbol.CROSS else Symbol.CIRCLE))
@@ -47,7 +47,7 @@ object TikTakToeService {
         DRAW,
         IN_PROGRESS,
         VICTORY_X,
-        VICTORY_Y,
+        VICTORY_O,
     }
 
 }
